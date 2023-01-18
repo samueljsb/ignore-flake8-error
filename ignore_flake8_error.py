5a1c23aa1d89ebe171fb16edc6abeb6a245b8133
Samuel Searles-Bryant <sam.searles-bryant@krakentechnologies.ltd>
2023-01-18 20:02:51 +0000

    Refactor to make easier to test in pieces

--- ignore_flake8_error.py
+++ ignore_flake8_error.py
@@ -11,16 +11,13 @@ from tokenize_rt import Token
 from tokenize_rt import tokens_to_src
 
 
-def main(argv: Sequence[str] | None = None) -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('code')
-    parser.add_argument('filenames', nargs='*')
-    args = parser.parse_args(argv)
-
-    # run flake8
-    print('=> running flake8', file=sys.stderr)
+def _run_flake8(code: str, filenames: Sequence[str]) -> dict[str, list[int]]:
     proc = subprocess.run(
-        ('flake8', '--select', args.code, *args.filenames),
+        (
+            'flake8', '--select', code,
+            '--format', '%(path)s %(row)s',
+            *filenames,
+        ),
         capture_output=True,
         text=True,
     )
@@ -28,43 +25,74 @@ def main(argv: Sequence[str] | None = None) -> int:
     # extract filenames and line numbers
     results: dict[str, list[int]] = defaultdict(list)
     for line in proc.stdout.splitlines():
-        [filename_, lineno_, *_] = line.split(':')
+        filename_, lineno_ = line.split()
         results[filename_].append(int(lineno_))
-    print(f'  found errors in {len(results)} files', file=sys.stderr)
 
-    for filename, linenos in results.items():
-        print('=> adding comments to', filename, file=sys.stderr)
+    return results
+
+
+def _add_code_to_comment(comment: str, code: str) -> str:
+    if 'noqa: ' in comment:
+        return comment.replace(
+            'noqa: ', f'noqa: {code},',
+        )
+    else:
+        return comment + f'  # noqa: {code}'
+
+
+def _add_comments(src: str, linenos: list[int], code: str) -> str:
+    lines = src.splitlines(keepends=True)
+
+    for lineno in linenos:
+        line = lines[lineno - 1]
+        tokens = src_to_tokens(line)
+
+        if tokens[-3].name == 'COMMENT':
+            old_comment = tokens[-3].src
+            new_comment = _add_code_to_comment(old_comment, code)
+            tokens[-3] = tokens[-3]._replace(src=new_comment)
+        else:
+            tokens = [
+                *tokens[:-2],
+                Token('UNIMPORTANT_WS', '  '),
+                Token('COMMENT', f'# noqa: {code}'),
+                *tokens[-2:],
+            ]
+
+        lines[lineno - 1] = tokens_to_src(tokens)
+
+    return ''.join(lines)
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('code')
+    parser.add_argument('filenames', nargs='*')
+    args = parser.parse_args(argv)
+
+    print('-> running flake8', file=sys.stderr)
+    violations = _run_flake8(args.code, args.filenames)
+
+    if not violations:
+        return 0
+
+    print(f'found violations in {len(violations)} files', file=sys.stderr)
+
+    print('-> adding noqa comments', file=sys.stderr)
+    ret = 0
+    for filename, linenos in violations.items():
+        print(filename, file=sys.stderr)
         with open(filename) as f:
-            lines = f.readlines()
+            src = f.read()
 
-        for lineno in linenos:
-            line = lines[lineno - 1]
-            tokens = src_to_tokens(line)
-
-            if tokens[-3].name == 'COMMENT':
-                old_comment = tokens[-3].src
-                if 'noqa: ' in old_comment:
-                    new_comment = old_comment.replace(
-                        'noqa: ', f'noqa: {args.code},',
-                    )
-                else:
-                    new_comment = old_comment + f'  # noqa: {args.code}'
-
-                tokens[-3] = tokens[-3]._replace(src=new_comment)
-            else:
-                tokens = [
-                    *tokens[:-2],
-                    Token('UNIMPORTANT_WS', '  '),
-                    Token('COMMENT', f'# noqa: {args.code}'),
-                    *tokens[-2:],
-                ]
-
-            lines[lineno - 1] = tokens_to_src(tokens)
+        src_with_comments = _add_comments(src, linenos, args.code)
 
         with open(filename, 'w') as f:
-            f.write(''.join(lines))
+            f.write(src_with_comments)
 
-    return 0
+        ret |= src_with_comments != src
+
+    return ret
 
 
 if __name__ == '__main__':
--- tests/ignore_flake8_error_test.py
+++ tests/ignore_flake8_error_test.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from ignore_flake8_error import main
 
 
-def test_ignore_flake8_error(tmp_path):
+def test_main(tmp_path):
     python_module = tmp_path / 't.py'
     python_module.write_text("""\
 import sys
@@ -12,11 +12,28 @@ from os import *  # noqa: F403
 from pathlib import *  # noqa: F403  # additional comment
 """)
 
-    main(('F401', str(python_module)))
+    ret = main(('F401', str(python_module)))
 
+    assert ret == 1
     assert python_module.read_text() == """\
 import sys  # noqa: F401
 from json import *  # additional comment  # noqa: F401
 from os import *  # noqa: F401,F403
 from pathlib import *  # noqa: F401,F403  # additional comment
 """
+
+
+def test_main_no_violations(tmp_path):
+    src = """\
+def foo():
+    print('hello there')
+"""
+
+    python_module = tmp_path / 't.py'
+    python_module.write_text(src)
+
+    ret = main(('F401', str(python_module)))
+
+    assert ret == 0
+
+    assert python_module.read_text() == src
