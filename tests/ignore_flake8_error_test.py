a51e62d27af0f8bbf95055db16783f2e84caaf8d
Samuel Searles-Bryant <sam@samueljsb.co.uk>
2023-05-05 15:08:53 +0100

    Add test to ensure we can handle existing ignores

--- tests/ignore_flake8_error_test.py
+++ tests/ignore_flake8_error_test.py
@@ -11,6 +11,7 @@ def test_main(tmp_path, capsys):
     python_module = tmp_path / 't.py'
     python_module.write_text("""\
 import sys
+import glob  # noqa: F401
 from json import *  # additional comment
 from os import *  # noqa: F403
 from pathlib import *  # noqa: F403  # additional comment
@@ -21,6 +22,7 @@ from pathlib import *  # noqa: F403  # additional comment
     assert ret == 1
     assert python_module.read_text() == """\
 import sys  # noqa: F401
+import glob  # noqa: F401
 from json import *  # additional comment  # noqa: F401
 from os import *  # noqa: F401,F403
 from pathlib import *  # noqa: F401,F403  # additional comment
